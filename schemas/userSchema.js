f9964b159c3a3e4798bfd7692ac1aa0af6bd523c
Administrator <Administrator@CHINA-20141029O>
2014-11-02 21:56:47 +0800

    1、增加登录注册功能 2、将路由部分代码抽出 3、配置开发环境下的变量

---
+++ schemas/userSchema.js
@@ -0,0 +1,64 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+var Schema = mongoose.Schema;
+
+var SALT_WORK_FACTOR = 10; //加密级别
+
+var UserSchema = new Schema({
+	name: {
+		type: String,
+		unique: true
+	},
+	password: String,
+	meta: {
+		createAt: {
+			type: Date,
+			default: Date.now()
+		},
+		updateAt: {
+			type: Date,
+			default: Date.now()
+		}
+	}
+});
+
+UserSchema.pre('save', function(next) {
+	var user = this;
+	if (this.isNew) {
+		this.meta.createAt = this.meta.updateAt = Date.now();
+	} else {
+		this.meta.updateAt = Date.now();
+	}
+
+	bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+		if (err) return next(err);
+
+		bcrypt.hash(user.password, salt, function(err, hash) {
+			if (err) return next(err);
+
+			user.password = hash;
+			next();
+		});
+	});
+});
+
+UserSchema.methods = {
+	comparePassword: function(_password, cb) {
+		bcrypt.compare(_password, this.password, function(err, isMatch) {
+
+			if (err) {
+				return cb(err);
+			}
+
+			cb(null, isMatch);
+		});
+	}
+};
+
+UserSchema.statics = {
+	fetch: function(cb) {
+		return this.find({}).sort('meta.updateAt').exec(cb);
+	}
+};
+
+module.exports = UserSchema;
